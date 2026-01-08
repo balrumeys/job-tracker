33bce573b105c708b61c17bd5260cbb48ebc2595
balrumeys <balrumeyst@gmail.com>
2026-01-08 23:32:06 +0300

    (feat) auth: add basic login form with state handling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+  }
   return (
     <div>
-      <h1>Login</h1>
-      <p>Login sayfası olacak</p>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
+
 export default Login;
